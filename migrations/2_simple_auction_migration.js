020d728935daf5b0b56da8c368b3282debb850cc
Ernest Irabor <irabeny89@gmail.com>
2022-09-30 11:39:47 +0100

    Refactor simple migration contract script

--- migrations/2_simple_auction_migration.js
+++ migrations/2_simple_auction_migration.js
@@ -1,1 +1,6 @@
-module.exports = async deployer => await deployer.deploy(artifacts.require("SimpleAuction"))
+module.exports = async (deployer, _, accounts) => await deployer.deploy(
+  artifacts.require("SimpleAuction"),
+  // 1min bidding period passed as 60sec
+  60,
+  accounts[0],
+)
